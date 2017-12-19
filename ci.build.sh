d988dbdac49c7353dbe7c72c256b86ce70ea57e4
Zeeko <vaezt@outlook.com>
2017-12-19 23:45:00 +0800

    添加 CI 支持

---
+++ ci.build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Build web site
+docker run -t --rm \
+    -v $(PWD):/app --workdir /app \
+    microsoft/aspnetcore-build \
+    bash -c "cd ./TooYoung.Web && dotnet restore && dotnet publish -c Release -o ./bin/Release/PublishOutput"
+
+# Build test
+cd ../Test
+docker run -t --rm \
+    -v $(PWD):/app --workdir /app \
+    microsoft/aspnetcore-build \
+    bash -c "cd ./Test && dotnet restore && dotnet test"
