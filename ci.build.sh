a32b261486cb02962154afca4d792f5ed5b9f0e0
Zeeko <vaezt@outlook.com>
2017-12-20 00:41:27 +0800

    调整构建脚本

--- ci.build.sh
+++ ci.build.sh
@@ -5,10 +5,10 @@ ls
 docker run -t --rm \
     -v $(pwd):/app --workdir /app \
     microsoft/aspnetcore-build \
-    sh -c "cd ./TooYoung.Web && dotnet restore --configfile ../Nuget.Config && dotnet publish -c Release -o ./bin/Release/PublishOutput"
+    sh -c "cd TooYoung.Web && dotnet restore --configfile ../Nuget.Config && dotnet publish -c Release -o ./bin/Release/PublishOutput"
 
 # Build test
 docker run -t --rm \
     -v $(pwd):/app --workdir /app \
     microsoft/aspnetcore-build \
-    sh -c "cd ./Test && dotnet restore --configfile ../Nuget.Config && dotnet test"
+    sh -c "cd Test && dotnet restore --configfile ../Nuget.Config && dotnet test"
