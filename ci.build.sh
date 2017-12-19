54ba3e32f6d86ffec988b27c95f16e5cc8ca629f
Zeeko <vaezt@outlook.com>
2017-12-20 00:03:52 +0800

    fix 'pwd not found'

--- ci.build.sh
+++ ci.build.sh
@@ -2,13 +2,13 @@
 
 # Build web site
 docker run -t --rm \
-    -v $(PWD):/app --workdir /app \
+    -v $(pwd):/app --workdir /app \
     microsoft/aspnetcore-build \
-    bash -c "cd ./TooYoung.Web && dotnet restore && dotnet publish -c Release -o ./bin/Release/PublishOutput"
+    ba -c "cd ./TooYoung.Web && dotnet restore && dotnet publish -c Release -o ./bin/Release/PublishOutput"
 
 # Build test
 cd ../Test
 docker run -t --rm \
     -v $(PWD):/app --workdir /app \
     microsoft/aspnetcore-build \
-    bash -c "cd ./Test && dotnet restore && dotnet test"
+    sh -c "cd ./Test && dotnet restore && dotnet test"
