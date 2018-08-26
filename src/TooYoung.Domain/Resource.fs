4467182e43202bb128f7c18eb81918395695f4c9
Zeeko <vaezt@outlook.com>
2018-08-26 15:36:20 +0800

    ♻️ Refactor domain model

---
+++ src/TooYoung.Domain/Resource.fs
@@ -0,0 +1,108 @@
+namespace TooYoung.Domain
+
+module Resource =
+    open System
+    open System.Collections.Generic
+    
+    /// 文件信息
+    type FileInfo(id: string, name: string) =
+        member val Id = id
+        member val Name = name with get, set
+
+        /// 文件大小
+        member val FileSize = 0 with get, set
+        member val BinaryId = "" with get, set
+        member val Metadatas = Dictionary()
+        member val Extension = "" with get, set
+
+    /// 文件二进制存储实体
+    type FileBinary(id: string) =
+        member val Id = id
+        member val Binary = Array.empty<byte> with get, set
+
+module Sharing =
+    open System
+    open System.Text.RegularExpressions
+    open Microsoft.FSharp.Quotations.Patterns
+    open Microsoft.FSharp.Reflection
+    open Resource
+
+    /// 来源引用规则
+    type RefererRule =
+        { AllowedHost: string }
+
+    /// Token 规则
+    type TokenRule =
+        { Token: string
+          ExpiredAt: DateTime option
+          Password: string }
+
+    /// 访问认证信息
+    type AccessClaim =
+        { Host: string option
+          Token: (string * DateTime * string) option }
+    
+    let rec isUnionCase =
+        function 
+        | Lambda(_, expr) | Let(_, _, expr) -> isUnionCase expr
+        | NewTuple exprs -> 
+            let iucs = List.map isUnionCase exprs
+            fun value -> List.exists ((|>) value) iucs
+        | NewUnionCase(uci, _) -> 
+            let utr = FSharpValue.PreComputeUnionTagReader uci.DeclaringType
+            box
+            >> utr
+            >> (=) uci.Tag
+        | _ -> failwith "Expression is no union case."
+
+    /// 测试来源规则是否满足
+    let refererRules host rule =
+        let regex = Regex(rule)
+        regex.IsMatch(host)
+
+    /// 测试 token 是否满足
+    let isTokenMatch (token, date, password) (rule: TokenRule) =
+                rule.Token = token
+                 && match rule.ExpiredAt with
+                    | Some expire -> expire >= date
+                    | None -> true
+                 && rule.Password = password
+
+    let rec canAccessVia test rules claim =
+        List.exists (test claim) rules
+
+    let rec canAccessViaReferer (allowedHosts: string list) (claim: string option) =
+        match claim with
+        | None -> false
+        | Some host -> canAccessVia refererRules allowedHosts host
+    
+    let rec canAccessViaSharingToken (tokens: TokenRule list) (claim: (string * DateTime * string) option) =
+        match claim with 
+        | None -> false
+        | Some token -> canAccessVia isTokenMatch tokens token
+
+    /// 文件分享信息
+    type SharingEntry(id: string, ownerId: string, resourceId: string) =
+        member val Id = id
+        member val ResourceId = resourceId
+        member val OwnerId = ownerId
+        member val TokenRules = List.empty<TokenRule> with get, set
+        member val RefererRules = List.empty<RefererRule> with get, set
+
+    let canAccess (entry: SharingEntry) (claims: AccessClaim) =
+        if entry.RefererRules.IsEmpty && entry.TokenRules.IsEmpty then true
+        else canAccessViaReferer
+                (entry.RefererRules |> List.map (fun x -> x.AllowedHost))
+                claims.Host
+             || canAccessViaSharingToken entry.TokenRules claims.Token
+
+
+module FileDirectory =
+    open Resource
+
+    open System
+    type FileDirectory(id: string, ownerId: string) =
+        member val Id = id
+        member val OwnerId = ownerId
+        member val DirectoryChildren = List.empty<FileDirectory> with get, set
+        member val FileChildren = List.empty<FileInfo> with get, set
---
+++ test/FsTest/SharingTests.fs
@@ -0,0 +1,73 @@
+module SharingTests
+
+open System
+open Xunit
+open TooYoung.Domain
+open Sharing
+open FluentAssertions
+
+type SharingTests() =
+    
+    [<Theory>]
+    [<MemberData("PublicEntryTestData")>]
+    member this.``Everyone can access public entry`` (claims: AccessClaim) =
+        /// 一个不包含任何认证规则的共享入口就是完全公开的
+        let entry = SharingEntry("2333", "owner", "file id")
+        
+        let result = claims |> canAccess entry
+        result.Should().BeTrue(null, null)
+    
+    static member PublicEntryTestData() =
+        let data = TheoryData<AccessClaim>()
+        [ { Host = Some "2333"; Token = Some ("some token", DateTime.Now, "some password") }
+          { Host = None; Token = Some ("some token", DateTime.Now, "") }
+        ]
+        |> List.iter data.Add
+        data
+    
+    static member RefererHostTestData() =
+        let data = TheoryData<AccessClaim, bool>()
+        [ { Host = Some "2333"; Token = None }, false
+          { Host = Some "gianthard.rocks"; Token = None }, true
+          { Host = Some "img.gianthard.rocks"; Token = None }, true
+          { Host = Some "2333.img.gianthard.rocks"; Token = None }, true
+          { Host = None; Token = None }, false
+          { Host = None; Token = Some ("2333", DateTime.Now, "password") }, false ]
+        |> List.iter data.Add
+        data
+    
+    [<Theory>]
+    [<MemberData("RefererHostTestData")>]
+    member this.``Referer host tests`` (claims: AccessClaim, expected: bool) =
+        let entry = SharingEntry("2333", "owner", "file id")
+        entry.RefererRules <- { AllowedHost = "(.+\.)*gianthard\.rocks" } :: entry.RefererRules
+        let result = claims |> canAccess entry
+        result.Should().Be(expected, null, null)
+    
+    static member TokenTestData() =
+        let data = TheoryData<AccessClaim, bool>()
+        [ { Host = Some "2333"; Token = None }, false
+          { Host = Some "gianthard.rocks"; Token = None }, false
+          { Host = Some "img.gianthard.rocks"
+            Token = Some ("some token", DateTime.Now, "Some password") }, false
+          { Host = None; Token = Some ("2333", DateTime(2018, 5, 1), "password") }, true
+          { Host = None; Token = Some ("23333", DateTime(2018, 5, 1), "password") }, false
+          { Host = None; Token = Some ("2333", DateTime(2018, 5, 1), "23333") }, false
+          { Host = None; Token = None }, false
+          { Host = None; Token = Some ("2333", DateTime(2019, 1, 1), "password") }, false ]
+        |> List.iter data.Add
+        data
+    
+    [<Theory>]
+    [<MemberData("TokenTestData")>]
+    member this.``Token tests`` (claims: AccessClaim, expected: bool) =
+        let entry = SharingEntry("2333", "owner", "file id")
+        
+        let tokenRule: TokenRule =
+            { Token = "2333"
+              ExpiredAt = DateTime(2018, 6, 1, 12, 0, 0) |> Some
+              Password = "password" }
+        entry.TokenRules <- tokenRule :: entry.TokenRules
+        
+        let result = claims |> canAccess entry
+        result.Should().Be(expected, null, null)
