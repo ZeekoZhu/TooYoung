081a334d146f3381ab6fe0d7ed90d780dcdd9a8d
Zeeko <vaezt@outlook.com>
2018-10-10 19:58:56 +0800

    ♻️ Replace nuget with paket

--- docs/api-gen.fsx
+++ docs/api-gen.fsx
@@ -1,22 +1,31 @@
+#load "../.paket/load/netstandard2.0/main.group.fsx"
+#r "netstandard"
 #load "../src/TooYoung.Domain/Domain.fs"
 #load "./api-gen-utils.fsx"
+#load "../src/WebCommon/Library.fs"
 open APIGenUtils
 open TooYoung.Domain
+open TooYoung.Domain.FileDirectory
 open TooYoung.Domain.Resource
 open TooYoung.Domain.Sharing
+open TooYoung.WebCommon.ErrorMessage
+
+let generalError code =
+    response code typeof<ErrorMessage>
 
 let testDoc =
     document
-        "Test" "/api"
-        "A test"
-        [ resource "TestItem" "/test"
-            [ request "Get All"
-                        [Auth; CORS]
-                        GET ""
+        "Too young" "/api"
+        "Web api for too young"
+        [ resource "Directory" "/dir"
+            [ request "Get root directory"
+                        [Auth]
+                        GET "/root"
                         []
-                        [ response 200 typeof<SharingEntry list> "list"
+                        [ response 200 typeof<FileDirectory> "Get root directory for current user"
+                          generalError 404 "Can not find root directory"
                         ]
-                        "Get all test item"
+                        "Get the root directory of current user"
               request "Get test"
                         [Auth; CORS]
                         GET "/{test}/{id}"
