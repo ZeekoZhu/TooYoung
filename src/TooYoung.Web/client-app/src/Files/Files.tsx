e1c08f7166d46ad919f17a3dbb8073ce6c46c8d8
stone <stone-lyl@outlook.com>
2018-12-28 22:30:53 +0800

    feat: 上传弹窗成功

--- src/TooYoung.Web/client-app/src/Files/Files.tsx
+++ src/TooYoung.Web/client-app/src/Files/Files.tsx
@@ -2,11 +2,12 @@ import './Files.less';
 
 import { observer } from 'mobx-react';
 import { Breadcrumb } from 'office-ui-fabric-react/lib/Breadcrumb';
+// import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
 import { DetailsList, DetailsListLayoutMode, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
+// import Dialog, { DialogFooter, DialogType } from 'office-ui-fabric-react/lib/Dialog';
 import { Panel, PanelType } from 'office-ui-fabric-react/lib/Panel';
 import React, { Component } from 'react';
-
 import { SharingPanel } from '../SharingPanel/SharingPanel';
 import { FilesStore } from './Files.store';
 
@@ -53,6 +54,22 @@ export class Files extends Component<FilesProps> {
                             ? <h1>请选择一个文件</h1>
                             : <SharingPanel entry={this.store.sharingEntry} />}
                     </Panel>
+                    <input className='input-file' ref={this.store.inputFileRef} type='file' />
+
+                    {/* <Dialog
+                        minWidth={400}
+                        hidden={!this.store.showUploadFlie.value}
+                        onDismiss={() => this.store.showUploadFlie.set(false)}
+                        >
+                        <div>
+                        </div>
+                        <DialogFooter>
+                            <PrimaryButton onClick={() =>
+                                this.store.showUploadFlie.set(false)} text='确定' />
+                            <DefaultButton onClick={() =>
+                                this.store.showUploadFlie.set(false)} text='取消' />
+                        </DialogFooter>
+                    </Dialog> */}
                 </div>
             </div>
         );
