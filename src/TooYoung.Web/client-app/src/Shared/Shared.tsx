bbf837de0c7043456cff572fad1ae3962a5d4d0a
stone <stone-lyl@outlook.com>
2018-12-28 23:09:46 +0800

    feat: 长的差不多的弹框*个

--- src/TooYoung.Web/client-app/src/Shared/Shared.tsx
+++ src/TooYoung.Web/client-app/src/Shared/Shared.tsx
@@ -1,6 +1,8 @@
 import { observer } from 'mobx-react';
+import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
 import { DetailsList, DetailsListLayoutMode, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
+import Dialog, { DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
 import React, { Component } from 'react';
 
 import { SharedStore } from './Shared.store';
@@ -40,7 +42,29 @@ export class Shared extends Component<SharedProps> {
                         enterModalSelectionOnTouch={true}
                     />
                 </div>
+                <Dialog
+                    onDismiss={this.closeCancelShareDialog}
+                    hidden={!this.store.showCancelShare.value}
+                    dialogContentProps={{
+                        title: '取消分享',
+                        subText: '是否取消分享？'
+                    }}
+                >
+                <DialogFooter>
+                    <PrimaryButton
+                        text='确定'
+                        onClick={this.closeCancelShareDialog}
+                    />
+                    <DefaultButton
+                        text='取消'
+                        onClick={this.closeCancelShareDialog}
+                    />
+                    </DialogFooter>
+                </Dialog>
             </div>
         );
+    }
+    private closeCancelShareDialog = (): void => {
+        this.store.showCancelShare.set(false);
     }
 }
