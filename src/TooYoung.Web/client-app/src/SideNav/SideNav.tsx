2422962d9c6c53043ca546accc16d4078e8c5b52
ZeekoZhu <vaezt@outlook.com>
2019-01-04 16:06:58 +0800

    🐛 Correct side bar items

--- src/TooYoung.Web/client-app/src/SideNav/SideNav.tsx
+++ src/TooYoung.Web/client-app/src/SideNav/SideNav.tsx
@@ -1,35 +1,53 @@
 import './SideNav.less';
 
-import { observer } from 'mobx-react';
+import { computed } from 'mobx';
+import { inject, observer } from 'mobx-react';
 import { INavLink, Nav } from 'office-ui-fabric-react/lib/Nav';
 import React, { Component } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+
+import { selectAppStore, WithAppStore } from '../Context';
+import { AuthStore } from '../stores/Auth.store';
 
 // tslint:disable-next-line:no-empty-interface
 interface ISideNavProps {
 }
 
-type SideNavProps = ISideNavProps & RouteComponentProps;
+type SideNavProps = ISideNavProps & RouteComponentProps & WithAppStore;
 
+@inject(selectAppStore)
 @observer
 class SideNavComp extends Component<SideNavProps> {
-    private links: INavLink[] = [
-        {
-            name: '文件管理',
-            url: '/files',
-            forceAnchor: true
-        },
-        {
-            name: '分享管理',
-            url: '/shared',
-            forceAnchor: true
-        },
-        {
-            name: '用户管理',
-            url: '/admin',
-            forceAnchor: true
+    authStore!: AuthStore;
+    @computed
+    get links() {
+        const links = [
+            {
+                name: '文件管理',
+                url: '/files',
+                forceAnchor: true
+            },
+            {
+                name: '分享管理',
+                url: '/shared',
+                forceAnchor: true
+            },
+        ];
+        if (this.authStore.isAdmin) {
+            links.push(
+                {
+                    name: '用户管理',
+                    url: '/admin',
+                    forceAnchor: true
+                }
+            );
         }
-    ];
+        return links;
+    }
+    constructor(props: SideNavProps) {
+        super(props);
+        this.authStore = props.appStore!.auth;
+    }
     public render() {
         return (
             <div className='side-nav'>
